09a5ffd3e9ea33d8d04db090d3102b5457c3ed46
Giacomo Pistolato <giacomopistolato1176@gmail.com>
2025-06-18 15:08:16 +0200

    beatgen e jazz finiti

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/home', to: 'home#index'
+  get '/beatgen', to: 'home#beatgen'
+  get '/destinazioni', to: 'home#destinazioni'
+  get '/jazz', to: 'home#jazz'
+  get '/murales', to: 'home#murales'
   root 'home#index' 
 end
